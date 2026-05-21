45eee5351a4a9d3399c35af9920fc3def6535d28
Felix Roe <fwroe@icloud.com>
2026-05-22 11:45:21 +1200

    added health labels to enemy and player

---
+++ scripts/components/health_component.gd
@@ -0,0 +1,34 @@
+extends Node
+class_name HealthComponent
+
+@export var health : int
+
+@export var max_health : int
+
+@export var shields : int
+
+@export var max_shields : int
+
+@export_group("labels")
+
+@export var shield_label : Label
+
+@export var health_label : Label
+
+@export var health_label_pivot : Control
+
+## handles the transform of a label, such as a health bar floating above an enemy
+@export var handle_label_transforms = false
+
+
+func _process(delta: float) -> void:
+	if max_shields != 0:
+		shield_label.text = str(shields, "/", max_shields, " shields")
+		
+	health_label.text = str(health, "/", max_health, " health")
+
+	if handle_label_transforms:
+		handle_label_gui()
+
+func handle_label_gui():
+	health_label_pivot.rotation = -get_parent().rotation
