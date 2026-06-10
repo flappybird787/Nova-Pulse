c6f3a4de7396b7e028de82945b57a3de31e47d52
Felix Roe <fwroe@icloud.com>
2026-06-11 11:44:48 +1200

    started adding missile upgrade

---
+++ scripts/node_controllers/missile.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+class_name Missile
+
+@export var damage = 0
+
+@export var speed = 0
+
+## set this to PLAYER or ENEMY
+@export var type : String
+
+func _physics_process(delta: float) -> void:
+	velocity = transform.y * speed
+	
+	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+
+
+func _on_player_area_body_entered(body: Node2D) -> void:
+	if type == "ENEMY":
+		
+		body.health_component.deal_damage(damage)
+		
+		queue_free()
+
+
+func _on_enemy_area_body_entered(body: Node2D) -> void:
+	if type == "PLAYER":
+		
+		body.health_component.deal_damage(damage)
+		
+		queue_free()
