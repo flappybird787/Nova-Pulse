d9a0db31d4504f032ea40280fa272601ceb311ef
Felix Roe <fwroe@icloud.com>
2026-06-15 17:19:46 +1200

    idk fixed a bug i think maybe i just did nothing thats a problem for future you

--- scripts/data/upgrades.gd
+++ scripts/data/upgrades.gd
@@ -1,17 +1,16 @@
 extends Node
 
 var all_upgrade_data = [
-	{"type": "HEALTH", "name": "Hull Booster", "description": "This upgrade uses reinforced alloys to boost the strength of the hull", "icon" : Image, "art": Image, "spread": 0},
-	{"type": "HEALTH", "name": "Hull Regen Booster", "description": "This upgrade uses nanobots to boost the regeneration of the hull", "icon" : Image, "art": Image, "spread": 0},
-	{"type": "SHIELD", "name": "Shield Booster", "description": "This upgrade uses better capacitors to boost the strength of the shield", "icon" : Image, "art": Image, "spread": 0},
+	{"type": "HEALTH", "name": "Hull Booster", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
+	{"type": "HEALTH", "name": "Hull Regen Booster", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
+	{"type": "SHIELD", "name": "Shield Booster", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
 	{"type": "SHIELD", "name": "Shield Regen Booster", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
 	{"type": "DAMAGE", "name": "Damage Booster", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
 	{"type": "SHIP", "name": "Speed Booster", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
 	{"type": "DAMAGE", "name": "Fire Rate Booster", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
-	{"type": "WEAPON", "name": "Missile", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 30},
+	{"type": "WEAPON", "name": "Missile", "description": "DONT USE THIS IN THE BOSS BATTLE IT WONT TRACK THE BOSS IDK WHY BUT I DONT WANNA FIX IT ITS 3AM IM 6 AND A HALF ENERGY DRINKS IN AND I CAN TASTE COLORS", "icon" : Image, "art": Image, "spread": 30},
 	{"type": "WEAPON", "name": "Railgun", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
 	{"type": "WEAPON", "name": "Turbolaser", "description": "lore.txt", "icon" : Image, "art": Image, "spread": 0},
-	
 ]
 
 @export var all_upgrades : Array[UpgradeBase] = []
