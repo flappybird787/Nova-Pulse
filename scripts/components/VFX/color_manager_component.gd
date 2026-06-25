e107b7409db6bb276f67847f473e1789bfe52904
Felix Roe <fwroe@icloud.com>
2026-06-25 12:47:01 +1200

    added VFX for the railgun

--- scripts/components/VFX/color_manager_component.gd
+++ scripts/components/VFX/color_manager_component.gd
@@ -3,7 +3,9 @@ class_name ColorManagerComponent
 
 @export var sprite : Node2D
 
-@export var trail : Line2D
+@export var trail : Node2D
+
+@export var transparent_effects : Node2D
 
 @export var color : Color
 
@@ -11,5 +13,9 @@ func _process(delta: float) -> void:
 	if sprite:
 		sprite.modulate = Color(color)
 	
+	if transparent_effects:
+		transparent_effects.modulate = Color(color)
+	
 	if trail:
 		trail.default_color = Color(color, 0.7)
+		
